a7396ec3f3ff1b9bf6ee9109f77d68e21f39cb00
yhg1024 <dgr1024kr@naver.com>
2023-11-30 17:55:12 +0900

    클론한거 commit check

--- src/chap_11/_02_Catch.java
+++ src/chap_11/_02_Catch.java
@@ -8,19 +8,23 @@ public class _02_Catch {
             // int[] arr = new int[3];
             // arr[5] = 100; // ArrayIndexOutOfBoundsException 인덱스 밖으로 벗어남
 
-            Object obj = "test";
-            System.out.println((int) obj); // ClassCastException 형변환 실패
+            // Object obj = "test";
+            // System.out.println((int) obj); // ClassCastException 형변환 실패
+
+            // String s = null;
+            // System.out.println(s.toLowerCase());
 
             // Exception 모든 예외들의 조상클래스
-        } catch (ArithmeticException e) {
+        } catch (ArithmeticException | ArrayIndexOutOfBoundsException e) { // 두개의 예외를 한꺼번에 쓰기 위해 사이에 | 쓰기
             System.out.println("뭔가 잘못 계산을 하셨네요.");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("인덱스를 잘못 설정했어요.");
+        //} catch (ArrayIndexOutOfBoundsException e) {
+        //    System.out.println("인덱스를 잘못 설정했어요.");
         } catch (ClassCastException e){
             System.out.println("잘못된 형 변환입니다.");
         } catch (Exception e) {
-            System.out.println("이런 문제가 발생했어요 => " + e.getMessage());
+            System.out.println("그 외의 모든 에러는 여기서 처리가 돼요.");
             e.printStackTrace();
         }
+        System.out.println("프로그램 정상 종료");
     }
 }
